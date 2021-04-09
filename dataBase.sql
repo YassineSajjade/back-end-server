130fdd7d75d94ee7ff61943a50ead6604603f0bc
YassineSajjade <yassinesajjade@gmail.com>
2021-04-09 12:55:33 +0100

    first push

---
+++ dataBase.sql
@@ -0,0 +1,38 @@
+INSERT INTO products VALUES (id,'Apple','Fresho','Fresh Meat','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...',
+12.00,'https://cdn.shopify.com/s/files/1/0108/7370/0415/products/Shop-18.png')
+
+INSERT INTO products VALUES (id,'Beetroot','Fresho','Vegetable','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...',
+50.00,'https://cdn.shopify.com/s/files/1/0108/7370/0415/products/Shop-22_df2f4015-e37d-4ae2-9fa8-876e15e78566.png')
+
+INSERT INTO products VALUES (id,'Elderberry','Koka','Fruits','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...',
+25.00,'https://cdn.shopify.com/s/files/1/0108/7370/0415/products/Shop-22_df2f4015-e37d-4ae2-9fa8-876e15e78566.png')
+
+INSERT INTO products VALUES (id,'Capsicum','Groca','Fruits','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...',
+20.00,'https://www.vhv.rs/dpng/d/444-4447149_pepper-png-image-bell-peppers-clipart-transparent-png.png')
+
+INSERT INTO products VALUES (id,'Coriander','Groca','Dry Fruit','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...',
+7.00,'https://cdn.shopify.com/s/files/1/0108/7370/0415/products/Shop-3.png')
+
+INSERT INTO products VALUES (id,'Cherry','Koka','Fruits','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...',
+35.00,'https://cdn.shopify.com/s/files/1/0108/7370/0415/products/Shop-21_420d48e4-ecf3-4ed0-a30c-1370f8094be2.png')
+
+INSERT INTO products VALUES (id,'Potato','fresho','Vegetable','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...',
+30.00,'https://cdn.shopify.com/s/files/1/0108/7370/0415/products/Shop-10_13a4d25e-54fe-4372-8c8e-6bf83c55a4f7.png')
+
+INSERT INTO products VALUES (id,'Mandrain Orange','Urban Harvest','Fruits','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...',
+28.00,'https://cdn.shopify.com/s/files/1/0108/7370/0415/products/Shop-22_df2f4015-e37d-4ae2-9fa8-876e15e78566.png')
+
+INSERT INTO products VALUES (id,'Watermelon','Groca','Fruits','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...',
+40.00,'https://cdn.shopify.com/s/files/1/0108/7370/0415/products/Shop-1.png')
+
+INSERT INTO products VALUES (id,'Broccoli','Vegetal','Vegetable','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...',
+34.00,'https://cdn.shopify.com/s/files/1/0108/7370/0415/products/Shop-15_8c94dda1-f20b-4dbc-a331-8c696e08b7e3.png')
+
+INSERT INTO products VALUES (id,'Cucumber','Koka','Vegetable','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...',
+21.00,'https://purepng.com/public/uploads/large/purepng.com-cucumbercucumbervegetablespicklegreenfoodhealthycucumbers-481522161925n6wbx.png')
+
+INSERT INTO products VALUES (id,'Coco Cookies','Stas','Cookies','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...',
+37.00,'https://cdn.shopify.com/s/files/1/0108/7370/0415/products/Shop-30_19053d26-edd6-4592-a8d7-791cd0961fc7.png')
+
+INSERT INTO products VALUES (id,'Nuts Cookies','Stas','Cookies','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat...',
+34.00,'https://cdn.shopify.com/s/files/1/0108/7370/0415/products/Shop-7.png')
---
+++ routes/api/products.js
@@ -0,0 +1,171 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../connection');
+
+db.connect( (err) => {
+    if(err) {
+        throw err;
+       //console.log("there is some err");
+    }else{
+        console.log('Mysql connected...');
+        
+        //Get all
+        router.get('/', (req, res) => {
+            db.query('SELECT * FROM products', (err, rows, fields) => {
+                if(err){
+                    res.send({ message: `There is some error => ${err}`});
+                    throw err;
+                }else{
+                    if(rows.length > 0){
+                        res.send(rows);
+                    }else{
+                        res.send({
+                        message: "there no data..."
+                        });
+                    }
+                    
+                }
+            });
+        });
+
+        //Get by #Type
+        router.get('/:type', (req, res) => {
+            db.query(`SELECT * FROM products WHERE productType = ?`,req.params.type, (err, rows, fields) => {
+                if(err){
+                    res.send({ message: `There is some error => ${err}` });
+                    throw err;
+                }else{
+                    if(rows.length > 0){
+                        res.send(rows);
+                    }else{
+                        res.send({ message: "there no data..." });
+                    }
+                    
+                }
+            });
+        });
+
+        // //Get by #Name
+        // app.get('/products/:name', (req, res) => {
+        //     db.query(`SELECT * FROM products WHERE name = ?`,req.params.name, (err, rows, fields) => {
+        //         if(err){
+        //             res.send({ message: `There is some error => ${err}` });
+        //             throw err;
+        //         }else{
+        //             if(rows.length > 0){
+        //                 res.send({
+        //                     data: rows
+        //                 });
+        //             }else{
+        //                 res.send({ message: "there no data..." });
+        //             }
+                    
+        //         }
+        //     });
+        // });
+
+        //Get by type & name
+        router.get('/:type/:name', (req, res) => {
+            db.query('SELECT * FROM products WHERE productType = ? AND name = ?',[req.params.type, req.params.name], (err, rows, fields) => {
+                if(err){
+                    throw err;
+                }else{
+                    if(rows.length > 0){
+                        res.send({
+                            data: rows
+                        });
+                    }else{
+                        res.send({
+                        message: `there is no ${req.params.name} match...`
+                        });
+                    }
+                    
+                }
+            });
+        });
+
+        //DELETE by id
+        router.delete('/:id', (req, res) => {
+            db.query('DELETE FROM products WHERE id = ?',[req.params.id], (err, rows, fields) => {
+                if(err){
+                    throw err;
+                }else{
+                    if(rows.affectedRows == 1){
+                        res.send({
+                            message: `Products with id ${req.params.id} has been deleted`,
+                            rowAffected: rows.affectedRows
+                            });
+                    }else{
+                        res.send({
+                            message: `There is no product with id ${req.params.id} !!`,
+                            affectedRows: rows.affectedRows,
+                            changedRows: rows.changedRows
+                            });
+                        }
+                    }
+                    
+                });
+            });
+
+        //CREATE
+        router.post('/', (req, res) => {
+
+            let sql = 'INSERT INTO products SET ?';
+            let product = req.body;
+            db.query(sql, product, (err, rows) => {
+                 if(err){
+                    res.send({
+                        message: `There is some error => ${err}`
+                        });
+                    throw err;
+                 }else{
+                         res.send({
+                             message: 'Product added with succed !!',
+                             affectedRows: rows.affectedRows,
+                             changedRows: rows.changedRows
+                             });
+                     }
+                  });
+            
+            });
+
+        //UPDATE
+        router.put('/:id', (req, res) => {
+
+            let product = req.body;
+            let sql = `UPDATE products SET name = '${product.name}', brand = '${product.brand}', productType = '${product.productType}', description = '${product.description}',
+                        prix = '${product.prix}', photo = '${product.photo}' WHERE id = '${req.params.id}'`;
+
+            db.query(sql, (err, rows) => {
+                 if(err){
+                    res.send({
+                        message: `There is some error => ${err}`
+                        });
+                    throw err;
+                 }else{
+                     if(rows.affectedRows == 1){
+                        res.send({
+                            message: `Product with id ${req.params.id} updated with succed !!`,
+                            affectedRows: rows.affectedRows,
+                            changedRows: rows.changedRows
+                            });
+                     }else{
+                        res.send({
+                            message: `Ther is no product with id ${req.params.id}  !!`,
+                        });
+                     }
+                        
+                     }
+                  });
+        
+            });
+            
+
+    
+
+
+    }
+    
+} );
+
+module.exports = router;
