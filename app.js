3568f9953a4942b3ee9703feffab25d6b9217ef2
YassineSajjade <yassinesajjade@gmail.com>
2021-04-19 12:10:00 +0000

    work with json file instead SQL

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const cors = require('cors');
+const moment = require('moment');
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded({ extended: true }) ); 
+
+//middleWare method
+const logger = (req,res,next) => {
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl} at ${moment().format()}`);
+    next();
+}
+
+//init middleWare
+app.use(logger);
 
 app.use('/products',require('./routes/api/products'));
 // app.use(cors());
--- routes/api/products.js
+++ routes/api/products.js
@@ -1,153 +1,226 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../../connection');
+//const db = require('../../connection');
+const products = require('../../data.json');
 
-db.connect( (err) => {
-    if(err) {
-        throw err;
-       //console.log("there is some err");
-    }else{
-        console.log('Mysql connected...');
+
+/* this method for using data FROM JSON file */
+    router.get('/', (req,res) => {
+        if(products.length > 0){
+            res.json({
+                products: products
+            });;
+        }else{
+            res.status(404).send({
+                message: 'There is no data !!'
+            })
+        }
+
         
-        //Get all
-        router.get('/', (req, res) => {
-            db.query('SELECT * FROM products', (err, rows, fields) => {
-                if(err){
-                    res.send({ message: `There is some error => ${err}`});
-                    throw err;
-                }else{
-                    if(rows.length > 0){
-                        res.send(rows);
+    })
+
+    router.get('/:type', (req,res) => {
+        const type = req.params.type;
+        const product = products.filter(prod => { return prod.productType.toLowerCase() === type });
+        if(product.length > 0){
+            res.send({
+                products: product
+            });
+        }else{
+            res.status(404).send({
+                message: 'Ther is no data !!'
+            })
+        }
+
+    })
+
+    router.get('/:type/:name', (req,res) => {
+        const type = req.params.type;
+        const name = req.params.name;
+        const product = products.filter(prod => 
+            { 
+                return prod.productType.toLowerCase() === type && prod.name.toLowerCase() === name
+            });
+        if(product.length > 0){
+            res.send({
+                product: product
+            });
+        }else{
+            res.status(404).send({
+                message: `No product with the name of ${name}`
+            })
+        }
+
+    })
+
+    router.delete('/:id', (req,res) => {
+        const id = req.params.id;
+        const product = products.filter(prod => 
+            { 
+                if(prod.id === parseInt(id)){
+                    delete prod.id;
+                }
+                return prod.id !== id 
+            });
+        if(product.length > 0){
+            res.send({
+                product: product
+            });
+        }else{
+            res.status(404).send({
+                message: `No product with the id of ${id}`
+            })
+        }
+
+    })
+
+/* this method for using data FROM MySql */
+// db.connect( (err) => {
+//     if(err) {
+//         throw err;
+//        //console.log("there is some err");
+//     }else{
+//         console.log('Mysql connected...');
+        
+//         //Get all
+//         router.get('/', (req, res) => {
+//             db.query('SELECT * FROM products', (err, rows, fields) => {
+//                 if(err){
+//                     res.send({ message: `There is some error => ${err}`});
+//                     throw err;
+//                 }else{
+//                     if(rows.length > 0){
+//                         res.send(rows);
                        
-                    }else{
-                        res.send({
-                        message: "there no data..."
-                        });
-                    }
+//                     }else{
+//                         res.send({
+//                         message: "there no data..."
+//                         });
+//                     }
                     
-                }
-            });
-            res.header('Access-Control-Allow-Origin', '*');
-            res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-            res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-        });
+//                 }
+//             });
+//             res.header('Access-Control-Allow-Origin', '*');
+//             res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//             res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//         });
 
-        //Get by #Type
-        router.get('/:type', (req, res) => {
-            db.query(`SELECT * FROM products WHERE productType = ?`,req.params.type, (err, rows, fields) => {
-                if(err){
-                    res.send({ message: `There is some error => ${err}` });
-                    throw err;
-                }else{
-                    if(rows.length > 0){
-                        res.send(rows);
-                    }else{
-                        res.send({ message: "there no data..." });
-                    }
+//         //Get by #Type
+//         router.get('/:type', (req, res) => {
+//             db.query(`SELECT * FROM products WHERE productType = ?`,req.params.type, (err, rows, fields) => {
+//                 if(err){
+//                     res.send({ message: `There is some error => ${err}` });
+//                     throw err;
+//                 }else{
+//                     if(rows.length > 0){
+//                         res.send(rows);
+//                     }else{
+//                         res.send({ message: "there no data..." });
+//                     }
                     
-                }
-            });
-        });
+//                 }
+//             });
+//         });
 
-        //Get by type & name
-        router.get('/:type/:name', (req, res) => {
-            db.query('SELECT * FROM products WHERE productType = ? AND name = ?',[req.params.type, req.params.name], (err, rows, fields) => {
-                if(err){
-                    throw err;
-                }else{
-                    if(rows.length > 0){
-                        res.send({
-                            data: rows
-                        });
-                    }else{
-                        res.send({
-                        message: `there is no ${req.params.name} match...`
-                        });
-                    }
+//         //Get by type & name
+//         router.get('/:type/:name', (req, res) => {
+//             db.query('SELECT * FROM products WHERE productType = ? AND name = ?',[req.params.type, req.params.name], (err, rows, fields) => {
+//                 if(err){
+//                     throw err;
+//                 }else{
+//                     if(rows.length > 0){
+//                         res.send({
+//                             data: rows
+//                         });
+//                     }else{
+//                         res.send({
+//                         message: `there is no ${req.params.name} match...`
+//                         });
+//                     }
                     
-                }
-            });
-        });
+//                 }
+//             });
+//         });
 
-        //DELETE by id
-        router.delete('/:id', (req, res) => {
-            db.query('DELETE FROM products WHERE id = ?',[req.params.id], (err, rows, fields) => {
-                if(err){
-                    throw err;
-                }else{
-                    if(rows.affectedRows == 1){
-                        res.send({
-                            message: `Products with id ${req.params.id} has been deleted`,
-                            rowAffected: rows.affectedRows
-                            });
-                    }else{
-                        res.send({
-                            message: `There is no product with id ${req.params.id} !!`,
-                            affectedRows: rows.affectedRows,
-                            changedRows: rows.changedRows
-                            });
-                        }
-                    }
+//         //DELETE by id
+//         router.delete('/:id', (req, res) => {
+//             db.query('DELETE FROM products WHERE id = ?',[req.params.id], (err, rows, fields) => {
+//                 if(err){
+//                     throw err;
+//                 }else{
+//                     if(rows.affectedRows == 1){
+//                         res.send({
+//                             message: `Products with id ${req.params.id} has been deleted`,
+//                             rowAffected: rows.affectedRows
+//                             });
+//                     }else{
+//                         res.send({
+//                             message: `There is no product with id ${req.params.id} !!`,
+//                             affectedRows: rows.affectedRows,
+//                             changedRows: rows.changedRows
+//                             });
+//                         }
+//                     }
                     
-                });
-            });
+//                 });
+//             });
 
-        //CREATE
-        router.post('/', (req, res) => {
+//         //CREATE
+//         router.post('/', (req, res) => {
 
-            let sql = 'INSERT INTO products SET ?';
-            let product = req.body;
-            db.query(sql, product, (err, rows) => {
-                 if(err){
-                    res.send({
-                        message: `There is some error => ${err}`
-                        });
-                    throw err;
-                 }else{
-                         res.send({
-                             message: 'Product added with succed !!',
-                             affectedRows: rows.affectedRows,
-                             changedRows: rows.changedRows
-                             });
-                     }
-                  });
+//             let sql = 'INSERT INTO products SET ?';
+//             let product = req.body;
+//             db.query(sql, product, (err, rows) => {
+//                  if(err){
+//                     res.send({
+//                         message: `There is some error => ${err}`
+//                         });
+//                     throw err;
+//                  }else{
+//                          res.send({
+//                              message: 'Product added with succed !!',
+//                              affectedRows: rows.affectedRows,
+//                              changedRows: rows.changedRows
+//                              });
+//                      }
+//                   });
             
-            });
+//             });
 
-        //UPDATE
-        router.put('/:id', (req, res) => {
+//         //UPDATE
+//         router.put('/:id', (req, res) => {
 
-            let product = req.body;
-            let sql = `UPDATE products SET name = '${product.name}', brand = '${product.brand}', productType = '${product.productType}', description = '${product.description}',
-                        prix = '${product.prix}', photo = '${product.photo}' WHERE id = '${req.params.id}'`;
+//             let product = req.body;
+//             let sql = `UPDATE products SET name = '${product.name}', brand = '${product.brand}', productType = '${product.productType}', description = '${product.description}',
+//                         prix = '${product.prix}', photo = '${product.photo}' WHERE id = '${req.params.id}'`;
 
-            db.query(sql, (err, rows) => {
-                 if(err){
-                    res.send({
-                        message: `There is some error => ${err}`
-                        });
-                    throw err;
-                 }else{
-                     if(rows.affectedRows == 1){
-                        res.send({
-                            message: `Product with id ${req.params.id} updated with succed !!`,
-                            affectedRows: rows.affectedRows,
-                            changedRows: rows.changedRows
-                            });
-                     }else{
-                        res.send({
-                            message: `Ther is no product with id ${req.params.id}  !!`,
-                        });
-                     }
+//             db.query(sql, (err, rows) => {
+//                  if(err){
+//                     res.send({
+//                         message: `There is some error => ${err}`
+//                         });
+//                     throw err;
+//                  }else{
+//                      if(rows.affectedRows == 1){
+//                         res.send({
+//                             message: `Product with id ${req.params.id} updated with succed !!`,
+//                             affectedRows: rows.affectedRows,
+//                             changedRows: rows.changedRows
+//                             });
+//                      }else{
+//                         res.send({
+//                             message: `Ther is no product with id ${req.params.id}  !!`,
+//                         });
+//                      }
                         
-                     }
-                  });
+//                      }
+//                   });
         
-            });
+//             });
             
-    }
+//     }
     
-} );
+// } );
 
 
 
