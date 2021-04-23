e11e631db3e514e08cfba0a98958570aa7322fda
YassineSajjade <yassinesajjade@gmail.com>
2021-04-23 15:46:00 +0000

    update

--- routes/api/products.js
+++ routes/api/products.js
@@ -20,9 +20,9 @@ const products = require('../../data/data.json');
                 message: 'There is no data !!'
             })
         }
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-        res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        // res.header('Access-Control-Allow-Origin', '*');
+        // res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        // res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
         
     });
